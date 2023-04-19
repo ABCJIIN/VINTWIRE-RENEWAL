20566ac93bcef125ad43b29e9221fc6160395b75
mmin0-0 <84059199+mmin0-0@users.noreply.github.com>
2023-04-19 10:47:12 +0900

    ,

--- vintwire2023/publishing/assets/js/include.js
+++
@@ -1,9 +0,0 @@
-    // INCLUDE 파일 공통요소 불러오는 스크립트
-    $(document).ready( function() {
-        
-      $("#header").load("../../../html/include/header.html"); // 헤더 INCLUDE  
-      $("#footer").load("../../../html/include/footer.html"); // 푸터 INCLUDE
-      $("#quickMenu").load("../../../html/include/quick-menu.html"); // 퀵메뉴 INCLUDE
-      $("#pagination").load("../../../html/include/pagination.html"); // 페이지네이션 INCLUDE
-
-    });
