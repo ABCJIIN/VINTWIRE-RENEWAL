f02930b726c35e7c802417f0795ec16b10187e80
mmin0-0 <84059199+mmin0-0@users.noreply.github.com>
2023-04-19 13:55:04 +0900

    、

---
+++ assets/js/include_index.js
@@ -0,0 +1,6 @@
+// INCLUDE 파일 공통요소 불러오는 스크립트
+$(document).ready( function() {
+    $("#header").load("html/include/header_index.html"); // 헤더 INCLUDE  
+    $("#footer").load("html/include/footer.html"); // 푸터 INCLUDE
+});
+  
