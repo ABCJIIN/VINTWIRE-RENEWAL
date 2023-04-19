d68f2f4d76be9033b516970d6ed5b25f52ba2465
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-04-19 11:07:35 +0900

    0419/11:07/jinny

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,7 +1,7 @@
 // INCLUDE 파일 공통요소 불러오는 스크립트
 $(document).ready( function() {
     
-    $("#header").load("/vintwire/html/include/header.html"); // 헤더 INCLUDE  
-    $("#footer").load("/vintwire/html/include/footer.html"); // 푸터 INCLUDE
+    $("#header").load("/html/include/header.html"); // 헤더 INCLUDE  
+    $("#footer").load("/html/include/footer.html"); // 푸터 INCLUDE
 });
   
